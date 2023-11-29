005c087c0ff0452378466412bf7e7a34901b813c
jose-lun <joselunaboja@gmail.com>
2023-11-29 14:47:35 -0800

    Fixed the projects page (new cover image & title)

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,10 +1,8 @@
-import Euler from "../assets/euler.png";
-import Combinatorics from "../assets/combinatorics.jpeg";
-import Mandelbrot from "../assets/mandelbrot.jpeg";
+import Taylor from "../assets/Taylor.png";
 
 export const ProjectList = [
     {
         name: "Taylor Series: Intuition and Applications",
-        image: Euler
+        image: Taylor
     }
 ]
